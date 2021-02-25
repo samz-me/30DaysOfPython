f2379aef069ed7d631a6ce815c16205a24615ae1
SamZ <samz.me@live.com>
2021-02-25 21:06:59 +0100

    day1

---
+++ day_1/helloworld.py
@@ -0,0 +1,29 @@
+import sys
+
+# 1
+print (sys.version)
+
+# 2
+print(2 + 3)             # addition(+)
+print(3 - 1)             # subtraction(-)
+print(2 * 3)             # multiplication(*)
+print(3 / 2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
+
+# 3
+print("Sam");
+print("Z");
+print("NL");
+print("I am enjoying 30 days of python");    
+    
+# 4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Sam"))
+print(type("Z"))
+print(type("NL"))
